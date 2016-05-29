b05e6581b31f0647a71e6033f8e4d8de81adb0c2
Horace Williams <horace.d.williams@gmail.com>
2016-05-28 23:06:19 -0600

    woo we can make http requests

--- src/cljs/clarke_wallet/core.cljs
+++ src/cljs/clarke_wallet/core.cljs
@@ -31,13 +31,14 @@
            "/home/worace/.wallet.der"
            "utf8"
            (fn [err data]
-             (println err data)
-             (reset! wallet-der data)))
+             (reset! wallet-der data)
+             (-> (doto (node-rsa) (.importKey @wallet-der "pkcs8"))
+                 (.sign (js/Buffer. "pizza") "base64"))))
 
-(def keypair
-  (doto (node-rsa) (.importKey @wallet-der "pkcs8")))
+;; (def keypair
+;;   (doto (node-rsa) (.importKey @wallet-der "pkcs8")))
 
-(.sign keypair (js/Buffer. "pizza") "base64")
+;; (.sign keypair (js/Buffer. "pizza") "base64")
 
 ;; (.sign (doto (node-rsa)
 ;;          (.importKey @wallet-der "pkcs8")) (js/Buffer. "pizza") "base64")
@@ -47,3 +48,49 @@
 ;; if none, prompt to create
 ;; connect to DNSServer to find available nodes
 ;; fetch balance for wallet addr
+
+
+;; function getTestPersonaLoginCredentials(callback) {
+
+;;     return http.get({
+;;         host: 'personatestuser.org',
+;;         path: '/email'
+;;     }, function(response) {
+;;         // Continuously update stream with data
+;;         var body = '';
+;;         response.on('data', function(d) {
+;;             body += d;
+;;         });
+;;         response.on('end', function() {
+
+;;             // Data reception is done, do whatever with it!
+;;             var parsed = JSON.parse(body);
+;;             callback({
+;;                 email: parsed.email,
+;;                 password: parsed.pass
+;;             });
+;;         });
+;;     });
+
+;; },
+
+(def http (js/require "http"))
+
+(defn read-json [string]
+  (->> string
+       (.parse js/JSON)
+       (js->clj)))
+
+(defn http-get [host path cb]
+  (println "HTTP Get" host path)
+  (.get http (clj->js {:host host :path path})
+        (fn [response]
+          (println "recv resp" response)
+          (let [body (atom [])]
+            (.on response "data" (fn [d]
+                                   (swap! body conj d)))
+            (.on response "error" (fn [e] println e))
+            (.on response "end" (fn []
+                                  (cb (read-json (apply str @body)))))))))
+
+(http-get "dns1.clarkecoin.org" "/api/peers" println)
