da0487852d90a6003409f90f202e3a768709db90
Horace Williams <horace.d.williams@gmail.com>
2016-05-31 10:10:13 -0600

    fixed copy/paste

--- app/app.js
+++ app/app.js
@@ -82,7 +82,20 @@ var debugMenu = {
   ]
 };
 
-var menuTemplate = [fileMenu, debugMenu, helpMenu];
+var editMenu =  {
+  label: "Edit",
+  submenu: [
+    { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+    { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+    { type: "separator" },
+    { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+    { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+    { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+    { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+  ]
+}
+
+var menuTemplate = [fileMenu, editMenu, debugMenu, helpMenu];
 
 
 // NOTE: not all of the browserWindow options listed on the docs page work
